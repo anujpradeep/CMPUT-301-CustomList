8f82ce2c46f3c6979508dcafdccfadd88ef9a830
anujpradeep <apradeep@ualberta.ca>
2021-10-28 18:01:53 -0600

    Update CustomListTest.java

--- app/src/test/java/CustomListTest.java
+++ app/src/test/java/CustomListTest.java
@@ -39,7 +39,8 @@ public class CustomListTest {
 
     @Test
     public void hasCityTest(){
-        list.addCity(mockCity());
-        assertTrue(list.hasCity(mockCity()));
+        City c = mockCity();
+        list.addCity(c);
+        assertTrue(list.hasCity(c));
     }
 }
